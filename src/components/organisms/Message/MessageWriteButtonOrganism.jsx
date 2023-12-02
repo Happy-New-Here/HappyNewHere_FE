2bb535b863f473a94204f685cfd703ac6271d559
joojjang <itchu999@naver.com>
2023-12-02 13:39:29 +0900

    [Feat]: 메시지쓰기버튼 완료

--- src/components/organisms/Message/MessageWriteButtonOrganism.jsx
+++ src/components/organisms/Message/MessageWriteButtonOrganism.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../common/Button";
+import envelopeIcon from "../../../assets/envelopeIcon.svg";
+import styled from "styled-components";
 
 const MessageWriteButtonOrganism = () => {
-  return <div>MessageWriteButtonOrganism</div>;
+  const navigate = useNavigate();
+
+  const handleMessageWriteButtonClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <Button
+      type="dottedline"
+      width="100%"
+      paddingY="16px"
+      fontSize="16px"
+      onClick={handleMessageWriteButtonClick}
+    >
+      <img src={envelopeIcon} alt="envelopeIcon" />
+      &nbsp;눌러서 메시지 쓰러 가기
+    </Button>
+  );
 };
 
 export default MessageWriteButtonOrganism;
