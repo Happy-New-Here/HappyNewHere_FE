009deb2dd0c4fdc2ea55fac12abb3db280f8efab
joojjang <itchu999@naver.com>
2023-12-02 13:45:28 +0900

    [Test]: 메시지쓰기버튼 테스트

--- src/components/template/MessageWriteTemplate.jsx
+++ src/components/template/MessageWriteTemplate.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TodayQuestion from "../organisms/Message/TodayQuestionOrganism";
+// import MessageWriteButtonOrganism from "../organisms/Message/MessageWriteButtonOrganism";
 import MessagePaperSelect from "../organisms/Message/MessagePaperThumbnailOrganism";
 import MessageWriteOrganism from "../organisms/Message/MessageWriteOrganism";
 import { ContentLayout } from "../../styles/utils";
@@ -10,6 +11,7 @@ const MessageWriteTemplate = () => {
       <div>프로필과 상태메시지</div>
       <ContentLayout>
         <TodayQuestion />
+        {/* <MessageWriteButtonOrganism /> */}
         <>
           <MessagePaperSelect />
           <MessageWriteOrganism />
