1f7d623e3bff75c3189361f3ba9a57715d062d23
joojjang <itchu999@naver.com>
2023-12-03 02:01:41 +0900

    [Refactor]: Logo 컴포넌트 생성

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,24 +1,11 @@
 import React from "react";
-import logo from "../../assets/logo.svg";
-import styled from "styled-components";
 import { HeaderLayout } from "../../styles/utils";
-
-const Logo = styled.img`
-  width: 120px;
-
-  @media (min-width: 768px) {
-    width: 130px;
-  }
-
-  @media (prefers-color-scheme: dark) {
-    filter: invert(100%);
-  }
-`;
+import Logo from "./Logo";
 
 const Header = () => {
   return (
     <HeaderLayout>
-      <Logo src={logo} alt="logo" />
+      <Logo width="120px" widthDesktop="130px" />
     </HeaderLayout>
   );
 };
