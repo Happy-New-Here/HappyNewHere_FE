670d383f3e95b13ec84feaddd70de40e041177e7
DingX2 <pourding@naver.com>
2023-12-05 04:04:33 +0900

    [Feat] 달력 컴포넌트 SVG 날짜에 맞춰 색깔변화 / tailwind 적용중

---
+++ src/components/common/Calendar.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import BellSVG from "./BellSVG";
+
+export const Calendar = {
+  wrapper: ({ children }) => {
+    return (
+      <div className="w-[352px] h-[41px] flex justify-between items-start">
+        {children}
+      </div>
+    );
+  },
+
+  getWeekDates: () => {
+    const today = new Date();
+    const day = today.getDay();
+
+    // 현재 요일이 월요일(1)이 아닌 경우, 이전 월요일로 이동
+    if (day !== 1) {
+      today.setDate(today.getDate() - day + 1);
+    }
+
+    const weekDates = [];
+    for (let i = 0; i < 7; i++) {
+      const date = new Date(today);
+      date.setDate(today.getDate() + i);
+      weekDates.push(date);
+    }
+
+    return weekDates;
+  },
+
+  // 날짜 하나
+  item: ({ day, date }) => {
+    // 현재 요일에 따라 type 값을 설정
+    let type = "navy";
+
+    switch (day % 7) {
+      case 0: // 일요일
+        type = "red";
+        break;
+      case 1: // 월요일
+        type = "navy";
+        break;
+      case 2: // 화요일
+        type = "white";
+        break;
+      case 3: // 수요일
+        type = "green";
+        break;
+      case 4: // 목요일
+        type = "maroon";
+        break;
+      case 5: // 금요일
+        type = "pink";
+        break;
+      case 6: // 토요일
+        type = "yellow";
+        break;
+      default:
+        break;
+    }
+
+    return (
+      <div className="w-8 h-[40.73px] flex flex-col justify-start items-start">
+        <BellSVG type={type} day={date} />
+      </div>
+    );
+  },
+};
--- src/components/template/AuthTemplate.jsx
+++ src/components/template/AuthTemplate.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import KakaoLoginBtn from "../common/kakaoLoginBtn";
-import { Calander } from "../common/Calander";
+import { Calendar } from "../common/Calendar";
 
 const AuthTemplate = () => {
+  const weekDates = Calendar.getWeekDates();
+  // console.log(weekDates[0].getDate());
+
   return (
     <>
       <KakaoLoginBtn />
       <div>
-        <Calander.wrapper>
-          <Calander.item day={18} />
-          <Calander.item day={19} />
-        </Calander.wrapper>
+        <Calendar.wrapper>
+          {weekDates.map((date, index) => (
+            <Calendar.item
+              key={index}
+              day={date.getDay()}
+              date={date.getDate()}
+            />
+          ))}
+        </Calendar.wrapper>
         로그인 템플릿 창입니다. 카카오 로그인 페이지입니다. 리다이렉트 URL
         설정할 때 template AuthRedirect.jsx로 해주세요~
       </div>
