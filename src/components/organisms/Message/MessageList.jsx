e6347a1175a2684a2dd84c78eebfd7cada2fd2bb
joojjang <itchu999@naver.com>
2023-12-06 09:02:42 +0900

    [Feat]: 메시지리스트 동적 렌더링, 오버플로우 처리

--- src/components/organisms/Message/MessageList.jsx
+++ src/components/organisms/Message/MessageList.jsx
@@ -1,39 +1,54 @@
 // MessageThumbnail 개수대로 동적 렌더링
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import MessageThumbnail from "../../common/MessageThumbnail";
-import axios from "axios";
-import { BASE_URL } from "../../../utils/URL";
+// import axios from "axios";
+// import { BASE_URL } from "../../../utils/URL";
 import styled from "styled-components";
 
-const StyledMessageList = styled.div`
+const StyledMessageList = styled.label`
   display: flex;
   flex-direction: column;
   gap: 16px;
+  overflow: auto;
 `;
 
 const MessageList = () => {
-  const userId = "mj"; // 임의
-  let params = {
-    // Header Authorization → jwt 토큰,
-  };
+  //   const userId = "mj"; // 조회할 아이디, 임의
+  //   let paramsToRequest = {
+  //     // Header Authorization → jwt 토큰,
+  //   };
+  //   const [responseData, setResponseData] = useState({
+  //     isOwner: false,
+  //     calanderStyle: "",
+  //     messages: [],
+  //   }); // 서버에서 가져온 데이터
 
-  useEffect(() => {
-    axios
-      .get(`${BASE_URL}/calender/${userId}`, params)
-      .then((response) => {
-        console.log(`The message list has been gotten successfuly.`);
-      })
-      .catch((error) => {
-        console.error(`An error occured while fetching the message list.`);
-      });
-  }, []);
+  //   useEffect(() => {
+  //     axios
+  //       .get(`${BASE_URL}/calender/${userId}`, paramsToRequest)
+  //       .then((response) => {
+  //         console.log(`The message list has been gotten successfuly.`);
+
+  //         const { isOwner, calanderStyle, messages } = response.data;
+  //         console.log(`isOwner: ${isOwner}, question: ${calanderStyle}, messages: ${messages}`);
+  //         setResponseData({ isOwner, calanderStyle, messages });
+  //       })
+  //       .catch((error) => {
+  //         console.error(`An error occured while fetching the message list.`);
+  //       });
+  //   }, []);
 
   return (
-    <StyledMessageList>
-      <MessageThumbnail paperNum="1" />
-      <MessageThumbnail paperNum="2" />
-      <MessageThumbnail paperNum="3" />
-    </StyledMessageList>
+    // <StyledMessageList>
+    //   <MessageThumbnail paperNum="1" />
+    // </StyledMessageList>
+    <>
+      {responseData.messages.map((message, index) => (
+        <StyledMessageList key={index}>
+          <MessageThumbnail paperNum={message["편지지 번호"]} />
+        </StyledMessageList>
+      ))}
+    </>
   );
 };
 
