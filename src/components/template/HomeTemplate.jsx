81e441cf3f727b54371b5e07708e3ba70cea3a29
joojjang <itchu999@naver.com>
2023-12-06 16:02:30 +0900

    [Test]: MessageViewOrganism 확인용

--- src/components/template/HomeTemplate.jsx
+++ src/components/template/HomeTemplate.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GiftBox from "../common/GiftBox";
 import MessageList from "../organisms/Message/MessageList";
+import MessageViewOrganism from "../organisms/Message/MessageViewOrganism";
 
 const HomeTemplate = () => {
   return (
@@ -8,6 +9,7 @@ const HomeTemplate = () => {
       홈 템플릿입니다.
       <GiftBox />
       <MessageList />
+      <MessageViewOrganism />
     </>
   );
 };
