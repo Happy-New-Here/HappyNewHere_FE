42f6dbe58684caa864b423095516c3ac00bd378e
joojjang <itchu999@naver.com>
2023-12-06 10:14:33 +0900

    [Chore]: 삭제 파일 임포트 삭제, 덮밥 복구

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,41 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import SearchPage from "./pages/Search";
-import NicknamePage from "./pages/Nickname";
+// import NicknamePage from "./pages/Nickname";
 import AuthPage from "./pages/Auth";
-import MailSelectPage from "./pages/MailSelect";
-import MailWritePage from "./pages/MailWrite";
+import FriendPage from "./pages/Friend";
 import RootLayout from "./pages/Root";
 import AuthRedirectPage from "./pages/AuthRedirectPage";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <RootLayout />,
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, path: "", element: <HomePage /> },
+      {
+        path: "auth",
         children: [
-            { index: true, path: '', element: <HomePage /> },
-            {
-                path: "auth",
-                children: [
-                    { path: "", element: <AuthPage /> },
-                    { path: "kakao/callback", element: <AuthRedirectPage /> },
-                    { path: "nickname", element: <NicknamePage /> },
-                ],
-            },
-            {
-                path: 'mail',
-                children: [
-                    { path: '', element: <MailSelectPage /> },
-                    { path: 'write', element: <MailWritePage /> },
-                ],
-            },
-            { path: 'search', element: <SearchPage /> },
+          { path: "", element: <AuthPage /> },
+          { path: "kakao/callback", element: <AuthRedirectPage /> },
+          // { path: "nickname", element: <NicknamePage /> },
         ],
-    },
+      },
+      {
+        path: "friend",
+        children: [{ path: "", element: <FriendPage /> }],
+      },
+      { path: "search", element: <SearchPage /> },
+    ],
+  },
 ]);
 
 function App() {
-    return (
-        <>
-            <RouterProvider router={router} />
-        </>
-    );
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
