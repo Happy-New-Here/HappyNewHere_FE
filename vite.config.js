ca27e98e0967e19906db5ffc90bb5feb6d8af323
joojjang <itchu999@naver.com>
2023-12-09 01:00:05 +0900

    [Build]: svgr 플러그인 설치

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +10,12 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+        plugins: ["babel-plugin-macros", "babel-plugin-styled-components"],
       },
     }),
+    svgr(), // svg를 리액트 컴포넌트화해줌
   ],
   define: {
-    'process.env': {}
-  }
+    "process.env": {},
+  },
 });
