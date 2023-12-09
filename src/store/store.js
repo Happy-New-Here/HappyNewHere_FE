5611049014d55a6d8da2a8e72afd3e69ac423bc9
Dobbymin <gmin0701@knu.ac.kr>
2023-12-09 18:07:47 +0900

    [Feat]: idInputSlice 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 
-import { AuthSlice } from "./auth-slice";
-import messageInputSlice from "./messageInputSlice";
-import selectedPaperNumSlice from "./SelectedPaperNumSlice";
-import isMessageWriteVisibleSlice from "./isMessageWriteVisibleSlice";
-import isAnonymousSlice from "./isAnonymousSlice";
-import searchSlice from "./searchSlice";
+import { AuthSlice } from './auth-slice';
+import messageInputSlice from './messageInputSlice';
+import selectedPaperNumSlice from './SelectedPaperNumSlice';
+import isMessageWriteVisibleSlice from './isMessageWriteVisibleSlice';
+import isAnonymousSlice from './isAnonymousSlice';
+import searchSlice from './searchSlice';
+import idInputSlice from './idInputSlice';
 
 export const store = configureStore({
-  reducer: {
-    auth: AuthSlice.reducer,
-    messageInput: messageInputSlice,
-    selectedPaperNum: selectedPaperNumSlice,
-    isMessageWriteVisible: isMessageWriteVisibleSlice,
-    isAnonymous: isAnonymousSlice,
-    search: searchSlice.reducer,
-  },
+    reducer: {
+        auth: AuthSlice.reducer,
+        messageInput: messageInputSlice,
+        selectedPaperNum: selectedPaperNumSlice,
+        isMessageWriteVisible: isMessageWriteVisibleSlice,
+        isAnonymous: isAnonymousSlice,
+        search: searchSlice.reducer,
+        idInput: idInputSlice,
+    },
 });
