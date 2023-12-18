7424594c9ba6e416144d43deeaa37d31a564f31e
joojjang <itchu999@naver.com>
2023-12-18 22:55:26 +0900

    [Feat]: 메시지 쓰러 가기 버튼 누를 시 로그인 여부 확인 후 넘기기

--- src/components/common/MessageWriteButton.jsx
+++ src/components/common/MessageWriteButton.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setIsMessageWriteVisible } from "../../store/isMessageWriteVisibleSlice";
 import { Button } from "../../components/common/Button";
@@ -18,12 +19,26 @@ const EnvelopeIcon = styled.img`
 
 const MessageWriteButton = () => {
   const [hover, setHover] = useState(false);
-
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const accessToken = localStorage.getItem("accessToken"); // 로컬스토리지의 엑세스토큰 불러오기
 
   const handleMessageWriteButtonClick = () => {
-    dispatch(setIsMessageWriteVisible(true));
+    if (accessToken) {
+      dispatch(setIsMessageWriteVisible(true));
+    } else {
+      // window.location.href(KAKAO_AUTH_URI);
+      navigate("/auth");
+    }
   };
+
+  const [messageWriteText, setMessageWriteText] = useState("로그인하고 메시지 쓰러 가기");
+  useEffect(() => {
+    if (accessToken) {
+      setMessageWriteText("메시지 쓰러 가기");
+    }
+  }, [messageWriteText]);
 
   return (
     <Button
@@ -41,7 +56,7 @@ const MessageWriteButton = () => {
       ) : (
         <EnvelopeIcon src={envelopeIconRed} alt="envelopeIcon" hover={hover} />
       )}
-      &nbsp;눌러서 메시지 쓰러 가기
+      &nbsp;{messageWriteText}
     </Button>
   );
 };
