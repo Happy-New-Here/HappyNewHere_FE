6c7f4e0f11a9caaf99e4f03b531c1f286bc06543
joojjang <itchu999@naver.com>
2023-12-20 04:50:21 +0900

    [Feat]: 홈 페이지 오픈일 이전

--- src/components/template/HomeTemplate.jsx
+++ src/components/template/HomeTemplate.jsx
@@ -9,13 +9,25 @@ import GiftBox from "../common/GiftBox";
 import { Calendar } from "../common/Calendar";
 import MessageList from "../organisms/Message/MessageList";
 import MessageViewOrganism from "../organisms/Message/MessageViewOrganism";
-
 import {
-  ResponsiveLayoutPC,
-  ResponsiveLayoutMobile,
-  InsideLayoutPC,
-  InsideLayoutMobile,
+  PlaceCenter,
+  ResponsiveLayout,
+  ContentLayout,
+  Leftside,
+  Center,
+  Rightside,
 } from "../../styles/utils";
+import { SmallText } from "../../styles/text";
+import styled from "styled-components";
+
+const StyledBeforeOpen = styled(PlaceCenter)`
+  flex-direction: column;
+  height: 100%;
+
+  // @media (min-width: 768px) {
+  //   height: auto;
+  // }
+`;
 
 const HomeTemplate = () => {
   const isPc = useMediaQuery({
@@ -38,44 +50,121 @@ const HomeTemplate = () => {
     localStorage.setItem("currentPage", JSON.stringify(currentPage));
   }, [currentPage]);
 
+  // 메시지 읽기 오픈일 설정
+  const targetDate = new Date("2023-12-18"); // 오픈일
+  const today = new Date(); // 오늘
+
   const handleDateClick = (date) => {
     setSelectedDate(date);
   };
 
+  const handleShareLink = () => {
+    // 내 메시지함 링크를 클립보드에 복사. 'navigator.clipboard' API 사용
+    const url = window.location.href;
+
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        // console.log(`링크 복사 성공: ${url}`);
+        alert(`링크가 클립보드에 복사되었어요. 이제 링크를 원하는 곳에 붙여넣을 수 있어요.`);
+      })
+      .catch((err) => {
+        console.error("링크 복사 실패:", err);
+      });
+  };
+
+  // 오픈일 이전
+  const BeforeOpen = () => {
+    return (
+      <StyledBeforeOpen>
+        <GiftBox />
+        <SmallText fontSize="20px" fontWeight="600" lineHeight="48px" textAlign="center">
+          메시지함은 1월 1일에 열 수 있어요! <br />
+        </SmallText>
+        <SmallText
+          fontSize="16px"
+          color="#9A0501"
+          lineHeight="24px"
+          textAlign="center"
+          cursor="pointer"
+          onClick={handleShareLink}
+        >
+          친구들에게 내 메시지함 링크 공유하기
+        </SmallText>
+      </StyledBeforeOpen>
+    );
+  };
+
+  // 오픈일 이후
+  const AfterOpen = () => {
+    return (
+      <>
+        <MessageList />
+        <MessageViewOrganism />
+        {selectedDate && (
+          <div>
+            {/* 선택한 날짜: {selectedDate.toDateString()} */}
+            {/* 선택한 날짜에 따른 메시지 목록 또는 내용을 여기에 표시 */}
+          </div>
+        )}
+      </>
+    );
+  };
+
   return (
-    <>
-      {isPc ? (
-        <ResponsiveLayoutPC>
-          <InsideLayoutPC>
+    <ResponsiveLayout>
+      {window.innerWidth >= 768 ? (
+        <>
+          <Leftside>
             <Header />
             <Footer currentPage="home" isPc={isPc} />
-          </InsideLayoutPC>
-        </ResponsiveLayoutPC>
+          </Leftside>
+          <ContentLayout>
+            <Center>
+              <Calendar.wrapper>
+                {weekDates.map((date, index) => (
+                  <Calendar.item
+                    key={index}
+                    day={date.getDay()}
+                    date={date.getDate()}
+                    onClick={() => handleDateClick(date)}
+                  />
+                ))}
+              </Calendar.wrapper>
+
+              {/* 오픈일 이후 메시지 목록 공개 */}
+              {today > targetDate ? <AfterOpen /> : <BeforeOpen />}
+            </Center>
+
+            <Rightside>
+              <Profile />
+            </Rightside>
+          </ContentLayout>
+        </>
       ) : (
         <>
           <Header />
-          <Calendar.wrapper>
-            {weekDates.map((date, index) => (
-              <Calendar.item
-                key={index}
-                day={date.getDay()}
-                date={date.getDate()}
-                onClick={() => handleDateClick(date)}
-              />
-            ))}
-          </Calendar.wrapper>
-          <GiftBox />
-          {selectedDate && (
-            <div>
-              선택한 날짜: {selectedDate.toDateString()}
-              {/* 선택한 날짜에 따른 메시지 목록 또는 내용을 여기에 표시 */}
-            </div>
-          )}
-          <MessageViewOrganism />
+          <ContentLayout>
+            <Profile />
+            <Calendar.wrapper>
+              {weekDates.map((date, index) => (
+                <Calendar.item
+                  key={index}
+                  day={date.getDay()}
+                  date={date.getDate()}
+                  onClick={() => handleDateClick(date)}
+                />
+              ))}
+            </Calendar.wrapper>
+
+            {/* 오픈일 이후 메시지 목록 공개 */}
+            {today > targetDate ? <AfterOpen /> : <BeforeOpen />}
+          </ContentLayout>
+
           <Footer currentPage="home" isPc={isPc} />
         </>
       )}
-    </>
+    </ResponsiveLayout>
   );
 };
 
