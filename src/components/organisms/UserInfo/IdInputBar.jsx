0e0db2011bae6c08c775ae35c12c2acea02efdee
Dobbymin <gmin0701@knu.ac.kr>
2023-12-21 14:23:42 +0900

    [Fix]: value 제거 및 주석 처리한 부분 제거

--- src/components/organisms/UserInfo/IdInputBar.jsx
+++ src/components/organisms/UserInfo/IdInputBar.jsx
@@ -1,4 +1,4 @@
-const IdInputBar = ({ value, onChange }) => {
+const IdInputBar = ({ onChange }) => {
     return (
         <>
             <div className="w-full h-12 p-2.5 mb-3 bg-white rounded-[10px] border border-red-800 justify-start items-center gap-2.5 inline-flex">
@@ -6,7 +6,6 @@ const IdInputBar = ({ value, onChange }) => {
                     className="outline-none text-sm ml-2 w-full"
                     type="id"
                     placeholder="아이디를 입력해주세요"
-                    value={value}
                     onChange={onChange}
                 />
             </div>
--- src/components/organisms/UserInfo/IdInputForm.jsx
+++ src/components/organisms/UserInfo/IdInputForm.jsx
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 export const IdInputForm = styled.div`
-    /* display: flex; */
-    /* flex-direction: column; */
     justify-content: center;
     align-items: center;
     margin-top: 1rem;
