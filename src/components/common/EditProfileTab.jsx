7ed7a4b2c7e755fc1f9db38816d7f7eb8613b8ab
Dobbymin <gmin0701@knu.ac.kr>
2023-12-23 02:44:40 +0900

    [Fix]: 마이페이지 css PC 수정

--- src/components/common/EditProfileTab.jsx
+++ src/components/common/EditProfileTab.jsx
@@ -1,53 +1,53 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import ProfileSubmitButton from "./ProfileSubmitButton";
-import backwardIcon from "../../assets/backwardIcon.svg";
-import { useMediaQuery } from "react-responsive";
-import styled from "styled-components";
-import { PlaceCenter, PlaceLeftRow, PlaceLeftColumn } from "../../styles/utils";
-import { SmallText } from "../../styles/text";
-import homeIcon from "../../assets/home.svg";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import ProfileSubmitButton from './ProfileSubmitButton';
+import backwardIcon from '../../assets/backwardIcon.svg';
+import { useMediaQuery } from 'react-responsive';
+import styled from 'styled-components';
+import { PlaceCenter, PlaceLeftRow, PlaceLeftColumn } from '../../styles/utils';
+import { SmallText } from '../../styles/text';
+import homeIcon from '../../assets/home.svg';
 
 const StyledEditProfileTab = styled(PlaceCenter)`
-  width: 100vw;
-  height: 50px;
-  padding: 0 24px;
-  justify-content: space-between;
-  //   border-bottom: 1px solid black; // 확인용
+    width: 100vw;
+    height: 50px;
+    padding: 0 24px;
+    justify-content: space-between;
+    //   border-bottom: 1px solid black; // 확인용
 
-  @media (min-width: 768px) {
-    width: 100%;
-    height: auto;
-    padding: 0 0;
-  }
+    @media (min-width: 768px) {
+        width: 100vw;
+        height: auto;
+        padding: 0 0;
+    }
 `;
 
 const StyledHomeIcon = styled.img`
-  width: 20px;
-  height: 20px;
+    width: 20px;
+    height: 20px;
 `;
 
 const EditProfileTab = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const isPc = useMediaQuery({
-    query: "(min-width:768px)",
-  });
+    const isPc = useMediaQuery({
+        query: '(min-width:768px)',
+    });
 
-  return (
-    <StyledEditProfileTab>
-      {isPc ? (
-        <SmallText fontSize="20px">프로필 편집</SmallText>
-      ) : (
-        <>
-          <img src={backwardIcon} alt="backwardIcon" onClick={() => navigate(-1)} />
-          <SmallText>프로필 편집</SmallText>
-          {/* <ProfileSubmitButton /> */}
-          <StyledHomeIcon src={homeIcon} alt="homeIcon" onClick={() => navigate("/")} />
-        </>
-      )}
-    </StyledEditProfileTab>
-  );
+    return (
+        <StyledEditProfileTab>
+            {isPc ? (
+                <SmallText fontSize="20px">프로필 편집</SmallText>
+            ) : (
+                <>
+                    <img src={backwardIcon} alt="backwardIcon" onClick={() => navigate(-1)} />
+                    <SmallText>프로필 편집</SmallText>
+                    {/* <ProfileSubmitButton /> */}
+                    <StyledHomeIcon src={homeIcon} alt="homeIcon" onClick={() => navigate('/')} />
+                </>
+            )}
+        </StyledEditProfileTab>
+    );
 };
 
 export default EditProfileTab;
--- src/styles/utils.jsx
+++ src/styles/utils.jsx
@@ -13,9 +13,9 @@ export const PlaceLeftRow = styled.div`
 `;
 
 export const PlaceRightRow = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
 `;
 
 export const PlaceLeftColumn = styled.div`
@@ -70,23 +70,23 @@ export const HeaderLayout = styled(PlaceLeftRow)`
 
 // 컨텐트 영역 레이아웃: 좌우 패딩 32px씩
 export const ContentLayout = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  width: 100%;
-  height: calc(100vh - 76px - 77px);
-  gap: 32px;
-  padding: 0 32px 32px; //상 좌우 하
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    width: 100%;
+    height: calc(100vh - 76px - 77px);
+    /* gap: 32px; */
+    padding: 0 32px 32px; //상 좌우 하
 
-  overflow: auto;
+    overflow: auto;
 
-  &::-webkit-scrollbar {
+    &::-webkit-scrollbar {
         width: 8px; /* Width of scrollbar */
         height: 0px; /* Set to 0 for horizontal scrollbar */
     }
 
     &::-webkit-scrollbar-thumb {
-        background-color: #C0C0C0; /* Scrollbar color */
+        background-color: #c0c0c0; /* Scrollbar color */
         border-radius: 4px; /* Round the corners of the scrollbar */
     }
 
@@ -94,10 +94,10 @@ export const ContentLayout = styled.div`
         background-color: #f1f1f1; /* Color of scrollbar track */
     }
 
-  @media (min-width: 768px) {
-    flex-direction: row;
-    padding: 0;
-  }
+    @media (min-width: 768px) {
+        flex-direction: row;
+        padding: 0;
+    }
 `;
 
 // 데탑 버전 좌, 중앙, 우
@@ -116,7 +116,6 @@ export const Center = styled.div`
         display: flex;
         flex-direction: column;
         justify-content: flex-start;
-        /* gsap: 32px; */
 
         // overflow: scroll;
     }
