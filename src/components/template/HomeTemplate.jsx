572d6d90c77271940a564c519ee60c97360b8dd2
Dobbymin <gmin0701@knu.ac.kr>
2023-12-24 02:19:36 +0900

    [HOTFIX!]: 홈 - 모바일 css 수정

--- src/components/template/HomeTemplate.jsx
+++ src/components/template/HomeTemplate.jsx
@@ -1,233 +1,212 @@
-import React, { useState, useEffect } from "react";
-import { useMediaQuery } from "react-responsive";
-import { useDispatch, useSelector } from "react-redux";
-import { setCurrentPage } from "../../store/currentPageSlice";
-import Header from "../common/Header";
-import Footer from "../common/Footer";
-import Profile from "../common/Profile";
-import GiftBox from "../common/GiftBox";
-import { Calendar } from "../common/Calendar";
-import MessageList from "../organisms/Message/MessageList";
-import MessageViewOrganism from "../organisms/Message/MessageViewOrganism";
-import MessageView from "../common/MessageView";
+import React, { useState, useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentPage } from '../../store/currentPageSlice';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+import Profile from '../common/Profile';
+import GiftBox from '../common/GiftBox';
+import { Calendar } from '../common/Calendar';
+import MessageList from '../organisms/Message/MessageList';
+import MessageViewOrganism from '../organisms/Message/MessageViewOrganism';
+import MessageView from '../common/MessageView';
 import {
-  PlaceCenter,
-  ResponsiveLayout,
-  ContentLayout,
-  Leftside,
-  Center,
-  Rightside,
-  ResponsiveLayoutPC,
-} from "../../styles/utils";
-import { SmallText } from "../../styles/text";
-import styled from "styled-components";
-import axios from "axios";
-import { BASE_URL } from "../../utils/URL";
-import {
-  setCalendar,
-  setMessagesList,
-  setOwner,
-} from "../../store/calendar-slice";
-import TodayQuestionOrganism from "../organisms/Message/TodayQuestionOrganism";
+    PlaceCenter,
+    ResponsiveLayout,
+    ContentLayout,
+    Leftside,
+    Center,
+    Rightside,
+    ResponsiveLayoutPC,
+} from '../../styles/utils';
+import { SmallText } from '../../styles/text';
+import styled from 'styled-components';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/URL';
+import { setCalendar, setMessagesList, setOwner } from '../../store/calendar-slice';
+import TodayQuestionOrganism from '../organisms/Message/TodayQuestionOrganism';
+import { ContentLayoutMobile } from '../organisms/Home/ContentLayoutMobile';
 
 const StyledBeforeOpen = styled(PlaceCenter)`
-  flex-direction: column;
-  height: 100%;
+    flex-direction: column;
+    height: 100%;
 
-  // @media (min-width: 768px) {
-  //   height: auto;
-  // }
+    // @media (min-width: 768px) {
+    //   height: auto;
+    // }
 `;
 
 const HomeTemplate = () => {
-  const isPc = useMediaQuery({
-    query: "(min-width:768px)",
-  });
+    const isPc = useMediaQuery({
+        query: '(min-width:768px)',
+    });
 
-  let weekDates = Calendar.getWeekDates(22); // 이벤트 시작날짜 설정
-  const [selectedDate, setSelectedDate] = useState(null);
-  const dispatch = useDispatch();
-  const currentPage = useSelector((state) => state.currentPage);
-  const userId = useSelector((state) => state.user.userId);
-  const selectedMessageList = useSelector(
-    (state) => state.calendar.messagesList
-  );
-  const selectedMessageIndex = useSelector(
-    (state) => state.calendar.selectedMessageIndex
-  );
+    let weekDates = Calendar.getWeekDates(22); // 이벤트 시작날짜 설정
+    const [selectedDate, setSelectedDate] = useState(null);
+    const dispatch = useDispatch();
+    const currentPage = useSelector((state) => state.currentPage);
+    const userId = useSelector((state) => state.user.userId);
+    const selectedMessageList = useSelector((state) => state.calendar.messagesList);
+    const selectedMessageIndex = useSelector((state) => state.calendar.selectedMessageIndex);
 
-  // currentPage(로그인 후 돌아올 페이지)를 설정하는 코드
-  // 최초 마운트시에(만) setCurrentPage를 디스패치
-  useEffect(() => {
-    dispatch(setCurrentPage("/"));
-  }, [dispatch]);
+    // currentPage(로그인 후 돌아올 페이지)를 설정하는 코드
+    // 최초 마운트시에(만) setCurrentPage를 디스패치
+    useEffect(() => {
+        dispatch(setCurrentPage('/'));
+    }, [dispatch]);
 
-  // 로컬스토리지에 currentPage 값을 저장 (앱 리렌더링 시에도 값 보존 위해서)
-  useEffect(() => {
-    localStorage.setItem("currentPage", JSON.stringify(currentPage));
-  }, [currentPage]);
+    // 로컬스토리지에 currentPage 값을 저장 (앱 리렌더링 시에도 값 보존 위해서)
+    useEffect(() => {
+        localStorage.setItem('currentPage', JSON.stringify(currentPage));
+    }, [currentPage]);
 
-  // 유저 메세지 전체 불러오기
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`${BASE_URL}/calender/${userId}`, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-          },
-        });
+    // 유저 메세지 전체 불러오기
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${BASE_URL}/calender/${userId}`, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+                    },
+                });
 
-        if (response.data) {
-          // 응답이 성공적으로 도착한 경우 데이터를 처리하고 Redux 상태를 업데이트
-          console.log("Response Data:");
-          console.log(response.data);
-          const data = response.data;
-          dispatch(setCalendar(data.calendar));
-          dispatch(setMessagesList(data.messagesList));
-          dispatch(setOwner(data.owner));
-        } else {
-          setTimeout(fetchData, 10000); // 10초 후에 다시 요청 (원하는 대기 시간으로 조절 가능)
-        }
-      } catch (error) {
-        // 에러 처리
-        alert("편지가 오는 중입니다. 새로고침으로 받아보세요 !");
-        console.error("Error fetching user messages:", error);
-      }
+                if (response.data) {
+                    // 응답이 성공적으로 도착한 경우 데이터를 처리하고 Redux 상태를 업데이트
+                    console.log('Response Data:');
+                    console.log(response.data);
+                    const data = response.data;
+                    dispatch(setCalendar(data.calendar));
+                    dispatch(setMessagesList(data.messagesList));
+                    dispatch(setOwner(data.owner));
+                } else {
+                    setTimeout(fetchData, 10000); // 10초 후에 다시 요청 (원하는 대기 시간으로 조절 가능)
+                }
+            } catch (error) {
+                // 에러 처리
+                alert('편지가 오는 중입니다. 새로고침으로 받아보세요 !');
+                console.error('Error fetching user messages:', error);
+            }
+        };
+
+        // 컴포넌트가 마운트될 때 최초 요청 시작
+        fetchData();
+    }, [userId, selectedDate]);
+    //selectedDate 는 임시로 넣어둬서 빼야함
+
+    // 메시지 읽기 오픈일 설정
+    const targetDate = new Date('2023-01-01'); // 오픈일
+    const today = new Date(); // 오늘
+
+    const handleDateClick = (date) => {
+        setSelectedDate(date);
     };
 
-    // 컴포넌트가 마운트될 때 최초 요청 시작
-    fetchData();
-  }, [userId, selectedDate]);
-  //selectedDate 는 임시로 넣어둬서 빼야함
+    const handleShareLink = () => {
+        // 내 메시지함 링크를 클립보드에 복사. 'navigator.clipboard' API 사용
+        const url = window.location.href;
 
-  // 메시지 읽기 오픈일 설정
-  const targetDate = new Date("2023-01-01"); // 오픈일
-  const today = new Date(); // 오늘
+        navigator.clipboard
+            .writeText(url)
+            .then(() => {
+                // console.log(`링크 복사 성공: ${url}`);
+                alert(`링크가 클립보드에 복사되었어요. 이제 링크를 원하는 곳에 붙여넣을 수 있어요.`);
+            })
+            .catch((err) => {
+                console.error('링크 복사 실패:', err);
+            });
+    };
 
-  const handleDateClick = (date) => {
-    setSelectedDate(date);
-  };
-
-  const handleShareLink = () => {
-    // 내 메시지함 링크를 클립보드에 복사. 'navigator.clipboard' API 사용
-    const url = window.location.href;
-
-    navigator.clipboard
-      .writeText(url)
-      .then(() => {
-        // console.log(`링크 복사 성공: ${url}`);
-        alert(
-          `링크가 클립보드에 복사되었어요. 이제 링크를 원하는 곳에 붙여넣을 수 있어요.`
+    // 오픈일 이전
+    const BeforeOpen = () => {
+        return (
+            <StyledBeforeOpen>
+                <GiftBox />
+                <SmallText fontSize="20px" fontWeight="600" lineHeight="48px" textAlign="center">
+                    메시지함은 1월 1일에 열 수 있어요! <br />
+                </SmallText>
+                <SmallText
+                    fontSize="16px"
+                    color="#9A0501"
+                    lineHeight="24px"
+                    textAlign="center"
+                    cursor="pointer"
+                    onClick={handleShareLink}
+                >
+                    친구들에게 내 메시지함 링크 공유하기
+                </SmallText>
+            </StyledBeforeOpen>
         );
-      })
-      .catch((err) => {
-        console.error("링크 복사 실패:", err);
-      });
-  };
+    };
 
-  // 오픈일 이전
-  const BeforeOpen = () => {
+    // 오픈일 이후
+    const AfterOpen = () => {
+        return (
+            <>
+                {selectedDate && <div>{/* 선택한 날짜에 따른 메시지 목록 또는 내용을 여기에 표시 */}</div>}
+                <MessageList messageList={selectedMessageList} selectedDate={selectedDate} />
+            </>
+        );
+    };
+
     return (
-      <StyledBeforeOpen>
-        <GiftBox />
-        <SmallText
-          fontSize="20px"
-          fontWeight="600"
-          lineHeight="48px"
-          textAlign="center"
-        >
-          메시지함은 1월 1일에 열 수 있어요! <br />
-        </SmallText>
-        <SmallText
-          fontSize="16px"
-          color="#9A0501"
-          lineHeight="24px"
-          textAlign="center"
-          cursor="pointer"
-          onClick={handleShareLink}
-        >
-          친구들에게 내 메시지함 링크 공유하기
-        </SmallText>
-      </StyledBeforeOpen>
-    );
-  };
-
-  // 오픈일 이후
-  const AfterOpen = () => {
-    return (
-      <>
-        {selectedDate && (
-          <div>
-            {/* 선택한 날짜에 따른 메시지 목록 또는 내용을 여기에 표시 */}
-          </div>
-        )}
-        <MessageList
-          messageList={selectedMessageList}
-          selectedDate={selectedDate}
-        />
-      </>
-    );
-  };
-
-  return (
-    <ResponsiveLayoutPC>
-      {isPc ? (
         <>
-          <Leftside>
-            <Header />
-            <Footer currentPage="home" isPc={isPc} />
-          </Leftside>
-          <ContentLayout>
-            <Center>
-              <Calendar.wrapper>
-                {weekDates.map((date, index) => (
-                  <Calendar.item
-                    key={index}
-                    day={date.getDay()}
-                    date={date.getDate()}
-                    onClick={() => handleDateClick(date)}
-                  />
-                ))}
-              </Calendar.wrapper>
+            {isPc ? (
+                <ResponsiveLayoutPC>
+                    <Leftside>
+                        <Header />
+                        <Footer currentPage="home" isPc={isPc} />
+                    </Leftside>
+                    <ContentLayout>
+                        <Center>
+                            <Calendar.wrapper>
+                                {weekDates.map((date, index) => (
+                                    <Calendar.item
+                                        key={index}
+                                        day={date.getDay()}
+                                        date={date.getDate()}
+                                        onClick={() => handleDateClick(date)}
+                                    />
+                                ))}
+                            </Calendar.wrapper>
 
-              {/* 오늘의 질문 */}
-              <TodayQuestionOrganism />
+                            {/* 오늘의 질문 */}
+                            <TodayQuestionOrganism />
 
-              {selectedMessageIndex ? <MessageViewOrganism /> : null}
-            </Center>
-          </ContentLayout>
+                            {selectedMessageIndex ? <MessageViewOrganism /> : null}
+                        </Center>
+                    </ContentLayout>
 
-          <Rightside>
-            <Profile />
-            {/* 오픈일 이후 메시지 목록 공개 */}
-            {today > targetDate ? <AfterOpen /> : <BeforeOpen />}
-          </Rightside>
+                    <Rightside>
+                        <Profile />
+                        {/* 오픈일 이후 메시지 목록 공개 */}
+                        {today > targetDate ? <AfterOpen /> : <BeforeOpen />}
+                    </Rightside>
+                </ResponsiveLayoutPC>
+            ) : (
+                <ResponsiveLayout>
+                    <Header />
+                    <ContentLayoutMobile>
+                        <Profile />
+                        <Calendar.wrapper>
+                            {weekDates.map((date, index) => (
+                                <Calendar.item
+                                    key={index}
+                                    day={date.getDay()}
+                                    date={date.getDate()}
+                                    onClick={() => handleDateClick(date)}
+                                />
+                            ))}
+                        </Calendar.wrapper>
+
+                        {/* 오픈일 이후 메시지 목록 공개 */}
+                        {today > targetDate ? <AfterOpen /> : <BeforeOpen />}
+                    </ContentLayoutMobile>
+
+                    <Footer currentPage="home" isPc={isPc} />
+                </ResponsiveLayout>
+            )}
         </>
-      ) : (
-        <>
-          <Header />
-          <ContentLayout>
-            <Profile />
-            <Calendar.wrapper>
-              {weekDates.map((date, index) => (
-                <Calendar.item
-                  key={index}
-                  day={date.getDay()}
-                  date={date.getDate()}
-                  onClick={() => handleDateClick(date)}
-                />
-              ))}
-            </Calendar.wrapper>
-
-            {/* 오픈일 이후 메시지 목록 공개 */}
-            {today > targetDate ? <AfterOpen /> : <BeforeOpen />}
-          </ContentLayout>
-
-          <Footer currentPage="home" isPc={isPc} />
-        </>
-      )}
-    </ResponsiveLayoutPC>
-  );
+    );
 };
 
 export default HomeTemplate;
