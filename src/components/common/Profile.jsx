f58b4a53efed096a2def58ad3fec80ed75c96d5f
joojjang <itchu999@naver.com>
2023-12-24 06:15:57 +0900

    [Style]: 데스크탑 화면에서 상태메시지 밑으로 빼기

--- src/components/common/Profile.jsx
+++ src/components/common/Profile.jsx
@@ -1,68 +1,95 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
-    StyledProfile,
-    ProfileImg,
-    NicknameAndStatemsg,
-    NicknameContainer,
-    Nickname,
-    StateMsg,
-    EditIcon,
-} from '../../styles/profileStyle';
-import { Img } from '../../styles/Img';
-import { useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { GetUserInfo } from '../../store/User-action';
-import editIcon from '../../assets/editIcon.svg';
+  StyledProfile,
+  ProfileImg,
+  ImgAndNickName,
+  NicknameAndStatemsg,
+  NicknameContainer,
+  Nickname,
+  StateMsg,
+  EditIcon,
+} from "../../styles/profileStyle";
+import { Img } from "../../styles/Img";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { GetUserInfo } from "../../store/User-action";
+import editIcon from "../../assets/editIcon.svg";
+import { useMediaQuery } from "react-responsive";
 
 const Profile = () => {
-    // const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const isPc = useMediaQuery({
+    query: "(min-width:768px)",
+  });
 
-    // const userId = useSelector((state) => state.user.userId);
-    // const nickname = useSelector((state) => state.user.nickname);
-    // const stateMsg = useSelector((state) => state.user.stateMsg);
-    // const profileImg = useSelector((state) => state.user.profileImg);
-    const userId = localStorage.getItem('userId');
-    const nickname = localStorage.getItem('nickname');
-    const stateMsg = localStorage.getItem('stateMsg');
-    const profileImg = localStorage.getItem('profileImg');
+  // const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        GetUserInfo(dispatch);
-        // 또는 아래와 같이 GetUserInfo 함수를 직접 호출할 수도 있습니다.
-        // const fetchUserInfo = async () => {
-        //   await GetUserInfo(dispatch);
-        // };
-        // fetchUserInfo();
-    }, [dispatch]);
+  // const userId = useSelector((state) => state.user.userId);
+  // const nickname = useSelector((state) => state.user.nickname);
+  // const stateMsg = useSelector((state) => state.user.stateMsg);
+  // const profileImg = useSelector((state) => state.user.profileImg);
+  const userId = localStorage.getItem("userId");
+  const nickname = localStorage.getItem("nickname");
+  const stateMsg = localStorage.getItem("stateMsg");
+  const profileImg = localStorage.getItem("profileImg");
 
-    const handleEditClick = () => {
-        // 프로필 편집 페이지로 이동
-        navigate('/editprofile');
-    };
+  useEffect(() => {
+    GetUserInfo(dispatch);
+    // 또는 아래와 같이 GetUserInfo 함수를 직접 호출할 수도 있습니다.
+    // const fetchUserInfo = async () => {
+    //   await GetUserInfo(dispatch);
+    // };
+    // fetchUserInfo();
+  }, [dispatch]);
 
-    return (
+  const handleEditClick = () => {
+    // 프로필 편집 페이지로 이동
+    navigate("/editprofile");
+  };
+
+  return (
+    <>
+      {isPc ? (
         <StyledProfile>
+          <ImgAndNickName>
             <Img
-                width="54px"
-                height="54px"
-                boxshadow="0px 0px 4px rgba(0, 0, 0, 0.25)"
-                borderradius="50%"
-                border="0.5px solid #909090"
-                src={profileImg}
+              width="54px"
+              height="54px"
+              boxshadow="0px 0px 4px rgba(0, 0, 0, 0.25)"
+              borderradius="50%"
+              border="0.5px solid #909090"
+              src={profileImg}
             />
-            <NicknameAndStatemsg>
-                <NicknameContainer>
-                    <Nickname>{nickname}</Nickname>
-                    {/* 편집아이콘 나일 땐 보이고 다른 사람일 땐 안 보이게
-          url을 확인을 하든 id를 확인을 하든 검증 로직 필요*/}
-                    <EditIcon src={editIcon} alt="edit icon" onClick={handleEditClick} />
-                </NicknameContainer>
-                {stateMsg && <StateMsg>{stateMsg}</StateMsg>}
-            </NicknameAndStatemsg>
+            <NicknameContainer>
+              <Nickname>{nickname}</Nickname>
+              <EditIcon src={editIcon} alt="edit icon" onClick={handleEditClick} />
+            </NicknameContainer>
+          </ImgAndNickName>
+          {stateMsg && <StateMsg>{stateMsg}</StateMsg>}
         </StyledProfile>
-    );
+      ) : (
+        <StyledProfile>
+          <Img
+            width="54px"
+            height="54px"
+            boxshadow="0px 0px 4px rgba(0, 0, 0, 0.25)"
+            borderradius="50%"
+            border="0.5px solid #909090"
+            src={profileImg}
+          />
+          <NicknameAndStatemsg>
+            <NicknameContainer>
+              <Nickname>{nickname}</Nickname>
+
+              <EditIcon src={editIcon} alt="edit icon" onClick={handleEditClick} />
+            </NicknameContainer>
+            {stateMsg && <StateMsg>{stateMsg}</StateMsg>}
+          </NicknameAndStatemsg>
+        </StyledProfile>
+      )}{" "}
+    </>
+  );
 };
 
 export default Profile;
--- src/styles/profileStyle.jsx
+++ src/styles/profileStyle.jsx
@@ -1,50 +1,65 @@
-import styled from 'styled-components';
-import { PlaceLeftColumn, PlaceLeftRow } from './utils';
-import { SmallText } from './text';
-import { Img } from './Img';
+import styled from "styled-components";
+import { PlaceLeftColumn, PlaceLeftRow } from "./utils";
+import { SmallText } from "./text";
+import { Img } from "./Img";
 
 export const StyledProfile = styled(PlaceLeftRow)`
-    width: 100%;
-    gap: 12px;
+  width: 100%;
+  gap: 10px;
 
-    margin-bottom: 1rem;
+  margin-bottom: 1rem;
+
+  @media (min-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
 `;
 
 export const ProfileImg = styled(Img)`
-    width: ${(props) => props.widthMobile || '54px'};
-    height: ${(props) => props.heightMobile || '54px'};
-    border-radius: 50%;
-    border: 0.5px solid #909090;
-    boxshadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
-    background: url(${(props) => props.backgroundImg}) center/cover;
+  width: ${(props) => props.widthMobile || "54px"};
+  height: ${(props) => props.heightMobile || "54px"};
+  border-radius: 50%;
+  border: 0.5px solid #909090;
+  boxshadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  background: url(${(props) => props.backgroundImg}) center/cover;
 
-    @media (min-width: 768px) {
-        width: ${(props) => props.widthDesktop || '46px'};
-        height: ${(props) => props.heightDesktop || '46px'};
-    }
+  @media (min-width: 768px) {
+    width: ${(props) => props.widthDesktop || "46px"};
+    height: ${(props) => props.heightDesktop || "46px"};
+  }
+`;
+
+export const ImgAndNickName = styled(PlaceLeftRow)`
+  gap: 10px;
+  font-style: normal;
+  line-height: normal;
 `;
 
 export const NicknameAndStatemsg = styled(PlaceLeftColumn)`
-    flex: 1; // Photo 뺀 나머지 공간 차지하도록
-    gap: 7px;
-    font-style: normal;
-    line-height: normal;
+  flex: 1; // Photo 뺀 나머지 공간 차지하도록
+  gap: 7px;
+  font-style: normal;
+  line-height: normal;
 `;
 
 export const NicknameContainer = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  @media (min-width: 768px) {
+    gap: 10px;
+  }
 `;
 
 export const Nickname = styled(SmallText)``;
 
 export const StateMsg = styled(SmallText)`
-    font-size: 12px;
-    color: #959595;
+  font-size: 12px;
+  color: #959595;
 `;
 
 export const EditIcon = styled.img`
-    cursor: pointer;
+  cursor: pointer;
 `;
