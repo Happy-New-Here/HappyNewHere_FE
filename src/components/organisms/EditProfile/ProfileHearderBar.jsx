8f20a5dfcb54ee59acaf2499462073684e488c30
joojjang <itchu999@naver.com>
2023-12-24 05:17:54 +0900

    [Style] StyledEditProfileTab 패딩값

--- src/components/organisms/EditProfile/ProfileHearderBar.jsx
+++ src/components/organisms/EditProfile/ProfileHearderBar.jsx
@@ -1,29 +1,27 @@
-import styled from 'styled-components';
-import { PlaceCenter } from '../../../styles/utils';
+import styled from "styled-components";
+import { PlaceCenter } from "../../../styles/utils";
 
 export const StyledEditProfileTab = styled(PlaceCenter)`
-    width: 100vw;
-    height: 50px;
-    padding: 0 24px;
-    justify-content: space-between;
-    //   border-bottom: 1px solid black; // 확인용
+  width: 100vw;
+  height: 50px;
+  padding: 0 24px;
+  justify-content: space-between;
+  //   border-bottom: 1px solid black; // 확인용
 
-    /* @media (min-width: 768px) {
-        width: 100%;
-        height: auto;
-        padding: 0 0;
-    } */
+  @media (min-width: 768px) {
+    padding: 0 0;
+  }
 `;
 
 export const StyledEditProfileTabMobile = styled(PlaceCenter)`
-    width: 100%;
-    height: auto;
-    padding: 1.5rem 1.5rem;
+  width: 100%;
+  height: auto;
+  padding: 1.5rem 1.5rem;
 
-    justify-content: space-between;
+  justify-content: space-between;
 `;
 
 export const StyledHomeIcon = styled.img`
-    width: 20px;
-    height: 20px;
+  width: 20px;
+  height: 20px;
 `;
